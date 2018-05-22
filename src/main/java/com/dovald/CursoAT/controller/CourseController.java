6282c8e63252f178de1cc8d59223eedfdf60c4c4
Dovald <jirachi_1000@hotmail.com>
2018-05-23 00:21:42 +0200

    Servicios REST

--- src/main/java/com/dovald/CursoAT/controller/CourseController.java
+++ src/main/java/com/dovald/CursoAT/controller/CourseController.java
@@ -1,14 +1,62 @@
 package com.dovald.CursoAT.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.Optional;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dovald.CursoAT.component.mapper.course.CourseMapper;
+import com.dovald.CursoAT.dto.CourseDTO;
+import com.dovald.CursoAT.model.Course;
 import com.dovald.CursoAT.service.CourseService;
 
-@Controller
+@RestController
+@RequestMapping(value = "/course")
 public class CourseController {
 	
 	@Autowired
 	CourseService courseService;
+	
+	@Autowired
+	CourseMapper courseMapper;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public Set<CourseDTO> findAll(@RequestParam(defaultValue = "0", required = false) Integer page,
+			@RequestParam(defaultValue = "10", required = false) Integer size) {
+		final Set<Course> users = courseService.findAll(PageRequest.of(page, size));
+		return courseMapper.modelToDto(users);
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	public CourseDTO findById(@PathVariable Integer id) {
+		final Optional<Course> user = courseService.findById(id);
+		return courseMapper.modelToDto(user.get());
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public CourseDTO create(@RequestBody CourseDTO dto) {
+		final Course user = courseMapper.dtoToModel(dto);
+		final Course createUser = courseService.create(user);
+		return courseMapper.modelToDto(createUser);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	public void update(@RequestBody CourseDTO dto) {
+		final Course user = courseMapper.dtoToModel(dto);
+		courseService.update(user);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE)
+	public void delete(@RequestBody CourseDTO dto) {
+		final Course user = courseMapper.dtoToModel(dto);
+		courseService.delete(user);
+	}
 
 }
--- src/main/java/com/dovald/CursoAT/controller/UserController.java
+++ src/main/java/com/dovald/CursoAT/controller/UserController.java
@@ -1,14 +1,63 @@
 package com.dovald.CursoAT.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.Optional;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dovald.CursoAT.component.mapper.user.UserMapper;
+import com.dovald.CursoAT.dto.UserDTO;
+import com.dovald.CursoAT.dto.UserPostDTO;
+import com.dovald.CursoAT.model.User;
 import com.dovald.CursoAT.service.UserService;
 
-@Controller
+@RestController
+@RequestMapping(value = "/user")
 public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	UserMapper userMapper;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public Set<UserDTO> findAll(@RequestParam(defaultValue = "0", required = false) Integer page,
+			@RequestParam(defaultValue = "10", required = false) Integer size) {
+		final Set<User> users = userService.findAll(PageRequest.of(page, size));
+		return userMapper.modelToDto(users);
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	public UserDTO findById(@PathVariable Integer id) {
+		final Optional<User> user = userService.findById(id);
+		return userMapper.modelToDto(user.get());
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public UserDTO create(@RequestBody UserPostDTO dto) {
+		final User user = userMapper.dtoToModel(dto);
+		final User createUser = userService.create(user);
+		return userMapper.modelToDto(createUser);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	public void update(@RequestBody UserPostDTO dto) {
+		final User user = userMapper.dtoToModel(dto);
+		userService.update(user);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE)
+	public void delete(@RequestBody UserPostDTO dto) {
+		final User user = userMapper.dtoToModel(dto);
+		userService.delete(user);
+	}
 
 }
