c7317d56565549d80d6c47371138a0b566ea7cae
Dovald <jirachi_1000@hotmail.com>
2018-05-23 22:48:28 +0200

    Añadido control de excepciones (ControllerAdvice)

---
+++ src/main/java/com/dovald/CursoAT/dto/ErrorDTO.java
@@ -0,0 +1,14 @@
+package com.dovald.CursoAT.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDTO {
+	
+	private String message;
+
+}
---
+++ src/main/java/com/dovald/CursoAT/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.dovald.CursoAT.exception;
+
+public class NotFoundException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final String MSG = "No se ha encontrado";
+	
+	public NotFoundException() {
+		super(MSG);
+	}
+	
+
+}
