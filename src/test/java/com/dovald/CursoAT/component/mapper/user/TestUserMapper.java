c24fb99d3165065d5187c56b420ba9c33187e5bf
Dovald <jirachi_1000@hotmail.com>
2018-05-28 20:47:13 +0200

    Ejemplo de test

---
+++ src/test/java/com/dovald/CursoAT/component/mapper/user/TestUserMapper.java
@@ -0,0 +1,39 @@
+package com.dovald.CursoAT.component.mapper.user;
+
+import org.dozer.DozerBeanMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.dovald.CursoAT.dto.UserDTO;
+import com.dovald.CursoAT.model.User;
+
+import junit.framework.Assert;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestUserMapper {
+	
+	@InjectMocks
+	UserMapper mapper = new UserMapperImpl();
+	
+	@Mock
+	DozerBeanMapper dozer;
+	
+	public void testModelToDTO() {
+		
+	}
+	
+	@Test
+	public void testDTOToModel() {
+		final User user = new User();
+		final UserDTO userDTO = new UserDTO();
+		userDTO.setName("Pepe");
+		Mockito.when(dozer.map(new UserDTO(), User.class)).thenReturn(user);		
+		final User res = mapper.dtoToModel(userDTO);
+		Assert.assertEquals("Pepe", res.getName());		
+	}
+
+}
