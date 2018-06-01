8ead17af9d9c8cf57478592574ee238596779165
Dovald <jirachi_1000@hotmail.com>
2018-06-01 22:43:15 +0200

    Hotfix

--- src/main/java/com/dovald/CursoAT/component/mapper/answers_by_user/Answers_by_UserMapperImpl.java
+++ src/main/java/com/dovald/CursoAT/component/mapper/answers_by_user/Answers_by_UserMapperImpl.java
@@ -1,9 +1,12 @@
 package com.dovald.CursoAT.component.mapper.answers_by_user;
 
+import org.springframework.stereotype.Component;
+
 import com.dovald.CursoAT.component.mapper.AbstractMapper;
 import com.dovald.CursoAT.dto.Answers_by_UserDTO;
 import com.dovald.CursoAT.model.Answers_by_User;
 
+@Component
 public class Answers_by_UserMapperImpl extends AbstractMapper<Answers_by_User, Answers_by_UserDTO> implements Answers_by_UserMapper {
 
 	@Override
