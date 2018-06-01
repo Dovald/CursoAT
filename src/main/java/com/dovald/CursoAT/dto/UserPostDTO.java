8d28045a142ddc2201964c191614c09a5106e5db
Dovald <jirachi_1000@hotmail.com>
2018-06-01 22:45:18 +0200

    hotfix

--- src/main/java/com/dovald/CursoAT/dto/UserPostDTO.java
+++ src/main/java/com/dovald/CursoAT/dto/UserPostDTO.java
@@ -1,8 +1,10 @@
 package com.dovald.CursoAT.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class UserPostDTO extends UserDTO {
 
 	public String password;
